15b9b0e3fcef911b64b5131ba867169d2e4988f7
apoztraph <raph.bur@gmail.com>
2017-06-06 21:06:18 +0200

    Changed the size of the window, created a minimal main file and a working game usable with love <3

--- conf.lua
+++ conf.lua
@@ -1,4 +1,5 @@
 function love.conf(t)
 	t.version = "0.10.2"
-	t.window.fullscreen = true
+	t.window.width = 1366
+	t.window.height = 768
 end
---
+++ main.lua
@@ -0,0 +1,17 @@
+-- variable contenant le niveau sur lequel on est
+currentLevel = 0
+
+-- fonction appelee au debut du jeu
+function love.load()
+
+end
+
+-- fonction appelee a chaque frame pour les calculs
+function love.update(dt)
+
+end
+
+-- fonction appelee a chaque frame pour les graphiques
+function love.draw(dt)
+
+end
